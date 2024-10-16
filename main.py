93e33ac6f5a21bc456a1a15e905eb4d75198c2e3
PrashantPulkit <prashantpulkit5@gmail.com>
2024-10-16 11:47:30 +0530

    updated local branch

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 
 print("Hello world")
-for i in range(100):
-    print(f"{i} Mississippi...")
-    print(f"{i + 1} Mississippi...")
+for i in range(10):
+    print(f"{i} missipi")
 
-    if i == 1:
-        print("Wait! Is that Mr. Udhappa juggling balls?")
-    elif i == 2:
-        print("And look! Mrs. Udhippi is dancing with a hula hoop!")
-    elif i == 3:
-        print("What a party! Someone get the snacks!")
-    elif i == 4:
-        print("I think I see a ball flying by!")
-        
-print("The countdown is over! Time to celebrate with cake!")
