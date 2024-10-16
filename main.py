c4c13fa88aa345095a71e34f8755aed8adeaf993
itsdakshjain <dakshjain1265@gmail.com>
2024-10-16 11:36:19 +0530

    final change

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 
 print("Hello world")
 for i in range(100):
-    print(f"{i} missipi")
-    print(f"{i+1} missipi")
+    print(f"{i} Mississippi...")
+    print(f"{i + 1} Mississippi...")
 
-
-    while i=1:
-        print("mr udhappa")
-
-    while i=1:
-        print("mrs udhippi")
+    if i == 1:
+        print("Wait! Is that Mr. Udhappa juggling balls?")
+    elif i == 2:
+        print("And look! Mrs. Udhippi is dancing with a hula hoop!")
+    elif i == 3:
+        print("What a party! Someone get the snacks!")
+    elif i == 4:
+        print("I think I see a ball flying by!")
+        
+print("The countdown is over! Time to celebrate with cake!")
