3d02a2f4eb828a0a75af05d0c6751213d387db50
PrashantPulkit <prashantpulkit5@gmail.com>
2024-10-16 12:10:50 +0530

    change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 
 print("Hello world")
 for i in range(10):
-    print(f"{i} missipi")
-print("Hello world")
-for i in range(10):
-    print(f"{i} missipi")
+        print(f"{i} missipi")
+        
 
